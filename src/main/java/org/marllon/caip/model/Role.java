d4c3b327eb6fc860996b68f41a65749ef6c122c8
MarllonAnisio <marllon.anisio@hotmail.com>
2026-03-16 13:54:16 -0300

    feat: add Role and User relationships

--- src/main/java/org/marllon/caip/model/Role.java
+++ src/main/java/org/marllon/caip/model/Role.java
@@ -1,15 +1,19 @@
 package org.marllon.caip.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 import java.util.Objects;
 
 @AllArgsConstructor
@@ -27,6 +31,9 @@ public class Role {
     @Column(nullable = false, length = 50, name = "name")
     private String name;
 
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
 
     @Override
     public boolean equals(Object o) {
--- src/main/java/org/marllon/caip/model/User.java
+++ src/main/java/org/marllon/caip/model/User.java
@@ -3,15 +3,20 @@ package org.marllon.caip.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
 import java.util.Objects;
 
 @AllArgsConstructor
@@ -55,6 +60,14 @@ public class User {
     public int hashCode() {
         return Objects.hashCode(registration);
     }
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "fk_user_id"),
+            inverseJoinColumns = @JoinColumn(name = "fk_role_id")
+    )
+    private List<Role> roles;
 
     @Override
     public String toString() {
