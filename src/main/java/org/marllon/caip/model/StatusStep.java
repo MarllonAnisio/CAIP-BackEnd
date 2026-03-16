c9f0981eaf7920f0f035157c71473f32c00f4769
MarllonAnisio <marllon.anisio@hotmail.com>
2026-03-16 12:49:16 -0300

    feat: add StatusStep Entity and anotations

--- src/main/java/org/marllon/caip/model/StatusStep.java
+++ src/main/java/org/marllon/caip/model/StatusStep.java
@@ -1,4 +1,62 @@
 package org.marllon.caip.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity(name = "tb_status_step")
 public class StatusStep {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "The name cannot be empty.")
+    @Column(unique = true, nullable = false, length = 50)
+    private String name;
+
+    @NotBlank(message = "The description cannot be empty.")
+    @Column(nullable = false, length = 255, name = "description")
+    private String description;
+
+    @NotBlank(message = "The color cannot be empty.")
+    @Column(nullable = false, name = "color")
+    private String color;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        StatusStep that = (StatusStep) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "StatusStep{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
 }
