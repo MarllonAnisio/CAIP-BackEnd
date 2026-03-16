504b14f32c05988f07380f184c78278b679d7389
MarllonAnisio <marllon.anisio@hotmail.com>
2026-03-16 12:48:37 -0300

    feat: add Position Embeddable and anotations

--- src/main/java/org/marllon/caip/model/Position.java
+++ src/main/java/org/marllon/caip/model/Position.java
@@ -1,4 +1,24 @@
 package org.marllon.caip.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Embeddable
 public class Position {
+
+    @Column(name = "position_x")
+    private Integer x;
+
+    @Column(name = "position_y")
+    private Integer y;
+
 }
