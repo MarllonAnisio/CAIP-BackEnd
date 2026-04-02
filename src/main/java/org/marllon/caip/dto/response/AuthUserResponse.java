4242a629445304b9fee93eadb67e343d7687889e
MarllonAnisio <marllon.anisio@hotmail.com>
2026-04-01 23:59:46 -0300

    feat: add AuthUserResponse DTO for authentication responses

---
+++ src/main/java/org/marllon/caip/dto/response/AuthUserResponse.java
@@ -0,0 +1,7 @@
+package org.marllon.caip.dto.response;
+
+public record AuthUserResponse ( String token,
+long expiresInSeconds,
+String usernameOrEmail,
+String refreshToken, long refreshExpirationSeconds) {
+}
