7a2273b59c1ff6560ec76ed2314c7905f6d90168
MarllonAnisio <marllon.anisio@hotmail.com>
2026-04-06 14:24:15 -0300

    refactor: update messages for clarity

--- src/main/java/org/marllon/caip/dto/request/UpdateUserRolesRequest.java
+++ src/main/java/org/marllon/caip/dto/request/UpdateUserRolesRequest.java
@@ -1,6 +1,10 @@
 package org.marllon.caip.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.List;
 
-public record UpdateUserRolesRequest(List<String> roles) {
-}
+public record UpdateUserRolesRequest(
+        @NotBlank(message = "The Roles cannot be empty.")
+        List<String> roles
+) {}
--- src/main/java/org/marllon/caip/dto/request/UserRequest.java
+++ src/main/java/org/marllon/caip/dto/request/UserRequest.java
@@ -3,12 +3,14 @@ package org.marllon.caip.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record UserRequest(@NotBlank(message = "Username é obrigatório")
-                          String username,
+public record UserRequest(
+        @NotBlank(message = "The Username cannot be empty.")
+        String username,
 
-                          @NotBlank(message = "Matrícula é obrigatória")
-                          String registration,
+        @NotBlank(message = "The Registration cannot be empty.")
+        String registration,
 
-                          @NotBlank(message = "Senha é obrigatória")
-                          @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres")
-                          String password) {}
+        @NotBlank(message = "The Password cannot be empty.")
+        @Size(min = 6, message = "The password must be at least 6 characters long.")
+        String password
+) {}
