c046d2e2c53f1348b3bc0ee8e1646bfdcd6afcd7
MarllonAnisio <marllon.anisio@hotmail.com>
2026-04-06 14:19:22 -0300

    feat: add NotBlank annotation to RefreshTokenRequest for validation

--- src/main/java/org/marllon/caip/dto/request/RefreshTokenRequest.java
+++ src/main/java/org/marllon/caip/dto/request/RefreshTokenRequest.java
@@ -1,4 +1,8 @@
 package org.marllon.caip.dto.request;
 
-public record RefreshTokenRequest(String refreshToken) {
-}
+import jakarta.validation.constraints.NotBlank;
+
+public record RefreshTokenRequest(
+        @NotBlank(message = "The refreshToken cannot be empty.")
+        String refreshToken
+) {}
