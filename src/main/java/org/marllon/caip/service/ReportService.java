c91cf598274c37e5b9f092dbdc619c9e52b78d76
MarllonAnisio <marllon.anisio@hotmail.com>
2026-04-12 13:54:09 -0300

    feat: add method to find report by id and map to response

--- src/main/java/org/marllon/caip/service/ReportService.java
+++ src/main/java/org/marllon/caip/service/ReportService.java
@@ -2,12 +2,16 @@ package org.marllon.caip.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.marllon.caip.dto.response.ReportResponse;
+import org.marllon.caip.model.Report;
 import org.marllon.caip.repository.ReportRepository;
 import org.marllon.caip.repository.StatusStepRepository;
 import org.marllon.caip.repository.UserRepository;
+import org.marllon.caip.service.mapper.ReportMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,10 +19,17 @@ public class ReportService {
 
     private static final Logger log = LoggerFactory.getLogger(ReportService.class);
     private final ReportRepository reportRepository;
-    private final ObjectMapper objectMapper;
+    private final ReportMapper reportMapper;
     private final StatusStepRepository statusStepRepository;
     private final UserRepository userRepository;
 
 
+    @Transactional(readOnly = true)
+    public ReportResponse findById(Long id) throws Exception {
+        Report report = reportRepository.findById(id)
+                .orElseThrow(() -> new Exception("Report not found with id: " + id));
+
+        return reportMapper.toResponse(report);
+    }
 
 }
