37df408a6a1ce296cb2a39116971bc6fece5f7c4
MarllonAnisio <marllon.anisio@hotmail.com>
2026-04-12 13:45:33 -0300

    feat: add repositories and dependencies to ReportService with logging support

--- src/main/java/org/marllon/caip/repository/StatusStepRepository.java
+++ src/main/java/org/marllon/caip/repository/StatusStepRepository.java
@@ -1,9 +1,12 @@
 package org.marllon.caip.repository;
 
 import org.marllon.caip.model.StatusStep;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface StatusStepRepository {
+@Repository
+public interface StatusStepRepository extends JpaRepository<StatusStep, Long> {
     Optional<StatusStep> findByName(String statusName);
 }
--- src/main/java/org/marllon/caip/service/ReportService.java
+++ src/main/java/org/marllon/caip/service/ReportService.java
@@ -1,6 +1,24 @@
 package org.marllon.caip.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.marllon.caip.repository.ReportRepository;
+import org.marllon.caip.repository.StatusStepRepository;
+import org.marllon.caip.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class ReportService {
+
+    private static final Logger log = LoggerFactory.getLogger(ReportService.class);
+    private final ReportRepository reportRepository;
+    private final ObjectMapper objectMapper;
+    private final StatusStepRepository statusStepRepository;
+    private final UserRepository userRepository;
+
 
 
 }
