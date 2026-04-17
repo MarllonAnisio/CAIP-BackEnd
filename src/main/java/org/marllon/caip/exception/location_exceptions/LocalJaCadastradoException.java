866791e9246fcf1b1c13d6bc4be03737d2e0bae4
MarllonAnisio <marllon.anisio@hotmail.com>
2026-04-17 05:02:27 -0300

    feat: add custom exceptions for duplicate and not-found location scenarios

---
+++ src/main/java/org/marllon/caip/exception/location_exceptions/LocalJaCadastradoException.java
@@ -0,0 +1,9 @@
+package org.marllon.caip.exception.location_exceptions;
+
+import org.marllon.caip.exception.BusinessRuleException;
+
+public class LocalJaCadastradoException extends BusinessRuleException {
+    public LocalJaCadastradoException(String message) {
+        super(message);
+    }
+}
