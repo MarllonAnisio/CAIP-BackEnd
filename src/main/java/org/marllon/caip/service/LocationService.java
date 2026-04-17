5efd70884dc7c1903383afaf79be4567273bb681
MarllonAnisio <marllon.anisio@hotmail.com>
2026-04-17 03:46:35 -0300

    feat: add LocationMapper for mapping entities and integrate caching into LocationService

--- src/main/java/org/marllon/caip/service/LocationService.java
+++ src/main/java/org/marllon/caip/service/LocationService.java
@@ -1,8 +1,12 @@
 package org.marllon.caip.service;
 
 import lombok.RequiredArgsConstructor;
+import org.marllon.caip.dto.response.LocationResponse;
 import org.marllon.caip.model.Location;
 import org.marllon.caip.repository.LocationRepository;
+import org.marllon.caip.service.mapper.LocationMapper;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,12 +14,19 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Service
+@CacheConfig(cacheNames = "locations")
 public class LocationService {
+
     private final LocationRepository locationRepository;
+    private final LocationMapper locationMapper;
 
     @Transactional(readOnly = true)
-    public List<Location> findAll() {
-        return locationRepository.findAll();
+    @Cacheable(key = "'all'") // Salva a lista na memória
+    public List<LocationResponse> findAll() {
+        return locationRepository.findAll()
+                .stream()
+                .map(locationMapper::toResponse)
+                .toList();
     }
     @Transactional(readOnly = true)
     public Location findById(Long id) {
