408f10014bf34396d1ff4f4335bb41a33c63ede3
MarllonAnisio <marllon.anisio@hotmail.com>
2026-04-18 16:43:26 -0300

    feat: add new endpoints for managing reports with RBAC and validation

--- src/main/java/org/marllon/caip/controller/ReportController.java
+++ src/main/java/org/marllon/caip/controller/ReportController.java
@@ -1,13 +1,18 @@
 package org.marllon.caip.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.marllon.caip.dto.request.ReportRequest;
 import org.marllon.caip.dto.response.ReportResponse;
 import org.marllon.caip.service.ReportService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -23,6 +28,35 @@ public class ReportController {
 
     private final ReportService reportService;
 
+    /**
+     *  Rotas publicas para uso de alunos logados
+     * */
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ReportResponse> findById(Long id) {
+        return ResponseEntity.ok(reportService.findById(id));
+    }
+
+    @GetMapping("/my-active-reports")
+    @PreAuthorize("hasAnyRole('ROLE_STUDENT', 'ROLE_LIBRARIAN', 'ROLE_ADMIN')")
+    public ResponseEntity<List<ReportResponse>> getMyReports() {
+        return ResponseEntity.ok(reportService.findMyActiveReports());
+    }
+
+    @GetMapping("/my-reports")
+    @PreAuthorize("hasAnyRole('ROLE_STUDENT', 'ROLE_LIBRARIAN', 'ROLE_ADMIN')")
+    public ResponseEntity<List<ReportResponse>> findMyReports() {
+        return ResponseEntity.ok(reportService.findMyReports());
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_STUDENT', 'ROLE_LIBRARIAN', 'ROLE_ADMIN')")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+        reportService.deleteReport(id);
+        return ResponseEntity.noContent().build();
+
+    }
 
     @PostMapping
     @PreAuthorize("hasRole('ROLE_STUDENT')")
@@ -36,28 +70,39 @@ public class ReportController {
         return ResponseEntity.created(uri).body(reportService.save(request));
     }
 
-    @PostMapping("/all-staff")
+    /**
+     * Rotas privadas
+     * */
+
+    @GetMapping("/all-staff")
     @PreAuthorize("hasAnyRole('ROLE_LIBRARIAN', 'ROLE_ADMIN')")
     public ResponseEntity<List<ReportResponse>> findAllForStaff() {
         return ResponseEntity.ok(reportService.findAllForStaff());
     }
 
-    @GetMapping("/my-reports")
-    @PreAuthorize("hasAnyRole('ROLE_STUDENT', 'ROLE_LIBRARIAN', 'ROLE_ADMIN')")
-    public ResponseEntity<List<ReportResponse>> findMyReports() {
-        return ResponseEntity.ok(reportService.findMyReports());
+    @GetMapping("/all-active")
+    @PreAuthorize("hasAnyRole('ROLE_LIBRARIAN', 'ROLE_ADMIN')")
+    public ResponseEntity<List<ReportResponse>> findAllActive() throws Exception {
+        return ResponseEntity.ok(reportService.findAllActive());
     }
 
-    @GetMapping("/my-active-reports")
-    @PreAuthorize("hasAnyRole('ROLE_STUDENT', 'ROLE_LIBRARIAN', 'ROLE_ADMIN')")
-    public ResponseEntity<List<ReportResponse>> findMyActiveReports() {
-        return ResponseEntity.ok(reportService.findMyActiveReports());
+
+    @GetMapping("/all-closed")
+    @PreAuthorize("hasAnyRole('ROLE_LIBRARIAN', 'ROLE_ADMIN')")
+    public ResponseEntity<List<ReportResponse>> findAllClosed() throws Exception {
+        return ResponseEntity.ok(reportService.findClosedReports());
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ReportResponse> findById(Long id) {
-        return ResponseEntity.ok(reportService.findById(id));
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_LIBRARIAN', 'ROLE_ADMIN')")
+    public ResponseEntity<ReportResponse> update(@PathVariable Long id, @RequestBody @Valid ReportRequest request) {
+        return ResponseEntity.ok(reportService.update(id, request));
     }
 
+    @PostMapping("/link-reports")
+    @PreAuthorize("hasRole('ROLE_LIBRARIAN')")
+    public ResponseEntity<ReportResponse> linkReports(Long perdidoId, Long encontradoId) {
+        return ResponseEntity.ok(reportService.linkReports(perdidoId, encontradoId));
+    }
 
 }
