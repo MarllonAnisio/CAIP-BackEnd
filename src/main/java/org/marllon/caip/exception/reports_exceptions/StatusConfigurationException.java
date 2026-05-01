a3f33be769beb47442dc3b66e7926da98681d403
MarllonAnisio <marllon.anisio@hotmail.com>
2026-05-01 18:35:00 -0300

    feat: add custom exceptions for invalid report match and status configuration scenarios

---
+++ src/main/java/org/marllon/caip/exception/reports_exceptions/StatusConfigurationException.java
@@ -0,0 +1,9 @@
+package org.marllon.caip.exception.reports_exceptions;
+
+import org.marllon.caip.exception.BusinessRuleException;
+
+public class StatusConfigurationException extends BusinessRuleException {
+    public StatusConfigurationException(String message) {
+        super(message);
+    }
+}
