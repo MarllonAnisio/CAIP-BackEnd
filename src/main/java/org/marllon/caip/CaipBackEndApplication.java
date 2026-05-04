f2651ff0aa676cf64de8a611f3c8eb740ca87c59
MarllonAnisio <marllon.anisio@hotmail.com>
2026-05-04 16:29:18 -0300

    feat: integrate Liquibase for database migrations, configure JPA settings, and refine pom.xml dependency structure

--- src/main/java/org/marllon/caip/CaipBackEndApplication.java
+++ src/main/java/org/marllon/caip/CaipBackEndApplication.java
@@ -3,11 +3,13 @@ package org.marllon.caip;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
+@EnableJpaRepositories(basePackages = "org.marllon.caip.repository")
 public class CaipBackEndApplication {
 
     public static void main(String[] args) {
