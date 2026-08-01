5a836ed2f39bbf38c171f0743e66a53afeac454b
MarllonAnisio <marllon.anisio@hotmail.com>
2026-08-01 19:19:01 -0300

    refactor: update role validation

--- src/main/java/org/marllon/caip/domains/user/dto/request/UpdateUserRolesRequest.java
+++ src/main/java/org/marllon/caip/domains/user/dto/request/UpdateUserRolesRequest.java
@@ -1,10 +1,11 @@
 package org.marllon.caip.domains.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
 public record UpdateUserRolesRequest(
-        @NotBlank(message = "The Roles cannot be empty.")
+        @NotNull(message = "role não pode ser null")
         List<String> roles
 ) {}
